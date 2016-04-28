1e3b7c1b422ea8e3e6f682794eba32125a5fb816
Cremz <cremzinc@gmail.com>
2016-04-28 08:43:17 +0200

    Fixed total page number when filtering

--- build/griddle.js
+++ build/griddle.js
@@ -350,7 +350,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	        this._resetSelectedRows();
 	    },
 	    componentWillReceiveProps: function (nextProps) {
-	        this.setMaxPage(nextProps.results);
+					var current_results = this.getCurrentResults();
+	        this.setMaxPage(current_results);
 
 	        if (nextProps.columns !== this.columnSettings.filteredColumns) {
 	            this.columnSettings.filteredColumns = nextProps.columns;
@@ -358,7 +359,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 	        if (nextProps.selectedRowIds) {
-	            var visibleRows = this.getDataForRender(this.getCurrentResults(), this.columnSettings.getColumns(), true);
+	            var visibleRows = this.getDataForRender(current_results, this.columnSettings.getColumns(), true);
 
 	            this.setState({
 	                isSelectAllChecked: this._getAreAllRowsChecked(nextProps.selectedRowIds, _.pluck(visibleRows, this.props.uniqueIdentifier)),
